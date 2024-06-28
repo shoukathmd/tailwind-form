0b45fd9f2b1d16563997af4028697c52a13a99e1
shoukathmd <shoukathmd@gmail.com>
2024-06-28 12:32:04 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          200: "#E0F1E8",
+          600: "#0C7D69",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
